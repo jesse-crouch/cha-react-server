456f27b73e825f21ceed4cd6316e0c98dab4086e
Jesse <jesse.t.crouch@gmail.com>
2020-03-09 21:03:11 -0400

    Changed the database connection to the remote server rather than localhost.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 
 const port = 3500;
 const DB_client = new Client({
-    host: 'localhost',
+    host: '99.242.212.59',
     port: 5432,
     database: 'CosgroveHockeyAcademy',
     user: 'postgres',
@@ -20,7 +20,6 @@ app.use('*', (req, res, next) => {
     next();
 });
 
-
 // Start - API
 app.get('/api/serverURL', (req, res) => {
     res.send({ server: 'http://localhost:3500' });
@@ -29,6 +28,7 @@ app.get('/api/serverURL', (req, res) => {
 app.get('/api/getServices', (req, res) => {
     (async function() {
         var query = 'select * from primary_service order by id asc;';
+        console.log('QUERY: ' + query);
         var result = await DB_client.query(query);
 
         if (result) {
@@ -42,12 +42,64 @@ app.get('/api/getServices', (req, res) => {
 app.post('/api/getSecondaryServices', (req, res) => {
     (async function() {
         var query = 'select * from secondary_service where primary_service_id = ' + req.body.service_id + ' order by id asc;';
+        console.log('QUERY: ' + query);
         var result = await DB_client.query(query);
 
         if (result) {
             res.send({ services: result.rows });
         } else {
             res.sendStatus(404);
+        }
+    })();
+});
+
+app.post('/api/getCalendarInfo', (req, res) => {
+    (async function() {
+        async function findFirst(currentDate) {
+            currentDate.setHours(0,0,0,0);
+            const start = currentDate.getTime();
+            currentDate.setDate(currentDate.getDate() + (6-currentDate.getDay()));
+            currentDate.setHours(23,59,0,0);
+            const end = currentDate.getTime();
+
+            var query = 'select *, extract(epoch from date) as epoch_date, concat(i.first_name, \' \', i.surname) as instructor_name from calendar_event, instructor i where secondary_id = ' + req.body.id + ' and i.id = instructor and (extract(epoch from date)*1000) between ' + start + ' and ' + end;
+            console.log('QUERY: ' + query);
+            const result = await DB_client.query(query);
+            currentDate.setHours(currentDate.getHours() + 1);
+            return (result.rowCount > 0) ? {
+                start: start,
+                events: result.rows
+            } : findFirst(currentDate);
+        }
+
+        // Need to compile service info
+        var query = 'select * from secondary_service where id = ' + req.body.id;
+        console.log('QUERY: ' + query);
+        var serviceResult = await DB_client.query(query);
+
+        // If this is a class, determine the first instance
+        var startDate = new Date(Number.parseInt(req.body.date));
+        startDate.setDate(startDate.getDate() - startDate.getDay());
+        startDate.setHours(0,0,0,0);
+
+        if (serviceResult.rows[0].type == 'class') {
+            var first = await findFirst(startDate);
+            res.send({
+                service_info: serviceResult.rows[0],
+                events: first.events,
+                startDate: first.start
+            });
+        } else {
+            // Check for events scheduled during the week
+            query = 'select *, extract(epoch from date) as epoch_date, concat(i.first_name, \' \', i.surname) as instructor_name from calendar_event, instructor i where secondary_id = ' + req.body.id + ' and i.id = instructor and (extract(epoch from date)*1000) between ' + startDate.getTime() + ' and ' + (startDate.getTime() + (1000*60*60*24*7));
+            console.log('QUERY: ' + query);
+            var eventResult = await DB_client.query(query);
+
+            res.send({
+                service_info: serviceResult.rows[0],
+                events: eventResult.rows,
+                startDate: startDate.getTime()
+            });
         }
     })();
 });
